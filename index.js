36a1143a832be15e5e5f0e646a2675030532954a
madison <madisonamoa@gmail.com>
2021-10-14 11:13:56 +0000

    post blog page, add post on database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
+const Blog = require('./models/BlogSchema')
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.set('view engine','ejs' )
 
 
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) =>{
@@ -37,6 +40,27 @@ app.get('/post', (req, res) =>{
 
 })
 
+app.get('/posts/new', (req, res) =>{
+
+    res.render('create')
+
+})
+
+app.post('/posts/store', async (req, res) =>{
+
+
+
+    await Blog
+    .create({
+        title: req.body.title,
+        body: req.body.body
+        })
+
+    res.redirect('/')
+
+
+})
+
 
 const PORT = 4000
 
@@ -47,12 +71,12 @@ app.listen(PORT, () =>{
 })
 
 
-// mongoose.connect('mongodb://localhost:27017/my_database', {useNewUrlParser: true}, () =>{
+mongoose.connect('mongodb://localhost:27017/my_database', {useNewUrlParser: true}, () =>{
 
-//     console.log(`[database] running`)
+    console.log(`[database] running`)
 
-// })
+})
 
-require('./test.js')
+// require('./test.js')
 
 
