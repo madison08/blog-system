016e11f114367ee5983bc632ed3ae3359363d366
madison <madisonamoa@gmail.com>
2021-10-21 07:18:26 +0000

    hashing user password

--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -1,11 +1,14 @@
 const User = require('../models/UserSchema')
 const path = require('path')
+const bcrypt = require('bcrypt')
 
 module.exports = (req, res) => {
 
+    // const passwordHash = bcrypt.hashSync
+
     User.create({
         username: req.body.username,
-        password: req.body.password
+        password: bcrypt.hashSync(req.body.password, 10)
     }).then((resp) => {
 
         console.log(resp)
